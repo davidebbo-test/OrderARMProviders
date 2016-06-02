de58a536d165dd716ba08d3964cbf13e6308947a
David Ebbo <david.ebbo@gmail.com>
2016-06-01 18:36:17 -0700

    Init

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OrderARMProviders
 {
@@ -10,6 +11,16 @@ namespace OrderARMProviders
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("OrderARMProviders input.json output.json");
+                return;
+            }
+            string jsonString = File.ReadAllText(args[0], Encoding.UTF8);
+            JObject obj = (JObject)JsonConvert.DeserializeObject(jsonString);
+            JArray providers = (JArray)obj["value"];
+            obj["value"] = new JArray(providers.OrderBy(provider => provider["id"]));
+            File.WriteAllText(args[1], JsonConvert.SerializeObject(obj, Formatting.Indented));
         }
     }
 }
